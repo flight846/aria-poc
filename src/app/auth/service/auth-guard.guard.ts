f3ae4e1ed9dd91b90e376c72cdeb54ba5944f734
yazid <yazid@decision-science.com>
2020-02-21 22:01:33 +0800

    Patch search component

--- src/app/auth/service/auth-guard.guard.ts
+++ src/app/auth/service/auth-guard.guard.ts
@@ -18,8 +18,7 @@ export class AuthGuard implements CanActivate {
     | boolean
     | UrlTree {
     return this.authService.isLoggedIn         // {1}
-      .pipe(
-        take(1),                              // {2}
+      .pipe(                             // {2}
         map((isLoggedIn: boolean) => {         // {3}
           if (!isLoggedIn) {
             this.router.navigate(['/login']);  // {4}
